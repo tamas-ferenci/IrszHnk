a375f07b61377806a0fdd96721ed0069812080bd
tamas-ferenci <tamas.ferenci@medstat.hu>
2020-02-10 00:53:27 +0100

    Frissítés a 2019-es állapotokra, és pár apróbb javítás.

--- IrszHnk.R
+++ IrszHnk.R
@@ -1,30 +1,33 @@
-library( XLConnect )
+options( java.parameters = "-Xmx4g" )
 
-IrszUrl <- "https://www.posta.hu/static/internet/download/Iranyitoszam-Internet.XLS"
-HnkUrl <- "http://www.ksh.hu/docs/hun/hnk/hnk_2016.xls"
+IrszUrl <- "https://www.posta.hu/static/internet/download/Iranyitoszam-Internet_uj.xlsx"
+HnkUrl <- "https://www.ksh.hu/docs/hun/hnk/hnk_2019.xlsx"
 
 temp <- tempfile()
 download.file( IrszUrl, temp, mode = "wb" )
-Irsz <- readWorksheetFromFile( temp, sheet = "Települések" )
+Irsz <- XLConnect::readWorksheetFromFile( temp, sheet = "Települések" )
 Irsz$Település <- trimws( Irsz$Település )
-Irsz$Település <- gsub( "*", "", Irsz$Település, fixed = TRUE )
-IrszTemp <- readWorksheetFromFile( temp, sheet = "Bp.u." )
-IrszTemp$Település <- paste0( "Budapest ", substring( IrszTemp$IRSZ, 2, 3 ), ". ker." )
+Irsz <- Irsz[ !grepl( "*", Irsz$Település, fixed = TRUE ), ]
+IrszTemp <- XLConnect::readWorksheetFromFile( temp, sheet = "Bp.u." )
+IrszTemp$KER[ IrszTemp$KER%in%c( "0", "Margitsziget" ) ] <- "XIII."
+IrszTemp$Település <- paste0( "Budapest ",
+                              formatC( as.numeric( as.roman( gsub( ".", "", IrszTemp$KER, fixed = TRUE ) ) ),
+                                       width = 2, flag = "0" ), ". ker." )
 IrszTemp$Településrész <- ""
 Irsz <- rbind( Irsz, IrszTemp[ , c( "IRSZ", "Település", "Településrész" ) ] )
-Irsz$Település[ Irsz$Település=="Budapest 00. ker." ] <- "Budapest"
 for ( t in c( "Miskolc", "Debrecen", "Szeged", "Pécs", "Győr" ) ) {
-  IrszTemp <- readWorksheetFromFile( temp, sheet = paste0( t, " u." ) )
+  IrszTemp <- XLConnect::readWorksheetFromFile( temp, sheet = paste0( t, " u." ) )
   names( IrszTemp )[ names( IrszTemp )=="IRSZ." ] <- "IRSZ"
   IrszTemp$Település <- t
   IrszTemp$Településrész <- ""
   Irsz <- rbind( Irsz, IrszTemp[ , c( "IRSZ", "Település", "Településrész" ) ] )
 }
 unlink( temp )
+Irsz <- Irsz[ !duplicated( Irsz ), ]
 
 temp <- tempfile()
 download.file( HnkUrl, temp, mode = "wb" )
-Hnk <- readWorksheetFromFile( temp, sheet = 1, startRow = 2 )
+Hnk <- XLConnect::readWorksheetFromFile( temp, sheet = 1 )
 Hnk <- Hnk[ !is.na( Hnk$Helység ), ]
 names( Hnk )[ 1:3 ] <- paste0( names( Hnk )[ 1 ], ".", Hnk[ 1, 1:3 ] )
 names( Hnk )[ 5:7 ] <- paste0( names( Hnk )[ 5 ], Hnk[ 1, 5:7 ] )
@@ -34,6 +37,8 @@ names( Hnk ) <- trimws( names( Hnk ) )
 Hnk <- Hnk[ -1, ]
 unlink( temp )
 
+Hnk <- Hnk[ !is.na( Hnk$Megye.megnevezése. ), ]
+
 names( Irsz )[ 2 ] <- names( Hnk )[ 1 ]
 
 IrszHnk <- merge( Irsz, Hnk, by = "Helység.megnevezése" )
@@ -41,3 +46,4 @@ IrszHnk <- merge( Irsz, Hnk, by = "Helység.megnevezése" )
 IrszHnk <- IrszHnk[ !duplicated( IrszHnk ), ]
 
 write.csv2( IrszHnk, "IrszHnk.csv", row.names = FALSE )
+saveRDS( IrszHnk, "IrszHnk.rds" )
