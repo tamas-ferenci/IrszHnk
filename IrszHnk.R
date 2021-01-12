7a3ae20e8e8e92c9910e602003d13ed2a23a5c83
Tamás <tamas.ferenci@medstat.hu>
2021-01-12 11:30:05 +0100

    Frissítés (2021-01-12 szerinti állapot).

--- IrszHnk.R
+++ IrszHnk.R
@@ -1,50 +1,39 @@
-options( java.parameters = "-Xmx4g" )
-
 IrszUrl <- "https://www.posta.hu/static/internet/download/Iranyitoszam-Internet_uj.xlsx"
 HnkUrl <- "https://www.ksh.hu/docs/hun/hnk/hnk_2019.xlsx"
 
-temp <- tempfile()
-download.file( IrszUrl, temp, mode = "wb" )
-Irsz <- XLConnect::readWorksheetFromFile( temp, sheet = "Települések" )
-Irsz$Település <- trimws( Irsz$Település )
-Irsz <- Irsz[ !grepl( "*", Irsz$Település, fixed = TRUE ), ]
-IrszTemp <- XLConnect::readWorksheetFromFile( temp, sheet = "Bp.u." )
-IrszTemp$KER[ IrszTemp$KER%in%c( "0", "Margitsziget" ) ] <- "XIII."
-IrszTemp$Település <- paste0( "Budapest ",
-                              formatC( as.numeric( as.roman( gsub( ".", "", IrszTemp$KER, fixed = TRUE ) ) ),
-                                       width = 2, flag = "0" ), ". ker." )
+temp <- tempfile(fileext = paste0(".",tools::file_ext(IrszUrl)))
+download.file(IrszUrl, temp, mode = "wb")
+Irsz <- rio::import(temp, sheet = "Települések")
+Irsz <- Irsz[!grepl("*", Irsz$Település, fixed = TRUE), ]
+IrszTemp <- rio::import(temp, sheet = "Bp.u.", guess_max = 2000)
+IrszTemp$KER[IrszTemp$KER%in%c("0", "Margitsziget")] <- "XIII."
+IrszTemp$Település <- paste0("Budapest ", formatC(as.numeric(as.roman(gsub(".", "", IrszTemp$KER, fixed = TRUE))),
+                                                  width = 2, flag = "0"), ". ker.")
 IrszTemp$Településrész <- ""
-Irsz <- rbind( Irsz, IrszTemp[ , c( "IRSZ", "Település", "Településrész" ) ] )
-for ( t in c( "Miskolc", "Debrecen", "Szeged", "Pécs", "Győr" ) ) {
-  IrszTemp <- XLConnect::readWorksheetFromFile( temp, sheet = paste0( t, " u." ) )
-  names( IrszTemp )[ names( IrszTemp )=="IRSZ." ] <- "IRSZ"
+Irsz <- rbind(Irsz, IrszTemp[ , c("IRSZ", "Település", "Településrész")])
+for (t in c("Miskolc", "Debrecen", "Szeged", "Pécs", "Győr")) {
+  IrszTemp <- rio::import(temp, sheet = paste0(t, " u."))
+  names(IrszTemp)[names(IrszTemp)=="IRSZ."] <- "IRSZ"
   IrszTemp$Település <- t
   IrszTemp$Településrész <- ""
-  Irsz <- rbind( Irsz, IrszTemp[ , c( "IRSZ", "Település", "Településrész" ) ] )
-  XLConnect::xlcFreeMemory()
+  Irsz <- rbind(Irsz, IrszTemp[ , c("IRSZ", "Település", "Településrész")])
 }
-unlink( temp )
-Irsz <- Irsz[ !duplicated( Irsz ), ]
+unlink(temp)
+Irsz <- Irsz[!duplicated(Irsz),]
 
-temp <- tempfile()
-download.file( HnkUrl, temp, mode = "wb" )
-Hnk <- XLConnect::readWorksheetFromFile( temp, sheet = 1 )
-Hnk <- Hnk[ !is.na( Hnk$Helység ), ]
-names( Hnk )[ 1:3 ] <- paste0( names( Hnk )[ 1 ], ".", Hnk[ 1, 1:3 ] )
-names( Hnk )[ 5:7 ] <- paste0( names( Hnk )[ 5 ], Hnk[ 1, 5:7 ] )
-names( Hnk )[ 8:9 ] <- paste0( names( Hnk )[ 8 ], ".", Hnk[ 1, 8:9 ] )
-names( Hnk )[ 14:26 ] <- paste0( names( Hnk )[ 14 ], ".", Hnk[ 1, 14:26 ] )
-names( Hnk ) <- trimws( names( Hnk ) )
-Hnk <- Hnk[ -1, ]
-unlink( temp )
+Hnk <- rio::import(HnkUrl, sheet = 1)
+Hnk <- Hnk[!is.na(Hnk$Helység),]
+names(Hnk)[1:3] <- paste0(names(Hnk)[1], ".", Hnk[1, 1:3])
+names(Hnk)[5:7] <- paste0(names(Hnk)[5], Hnk[1, 5:7])
+names(Hnk)[8:9] <- paste0(names(Hnk)[8], ".", Hnk[1, 8:9])
+names(Hnk)[14:26] <- paste0(names(Hnk)[14], ".", Hnk[1, 14:26])
+Hnk <- Hnk[-1, ]
 
-Hnk <- Hnk[ !is.na( Hnk$Megye.megnevezése. ), ]
+names(Irsz)[2] <- names(Hnk)[1]
 
-names( Irsz )[ 2 ] <- names( Hnk )[ 1 ]
+IrszHnk <- merge(Irsz, Hnk, by = "Helység.megnevezése")
 
-IrszHnk <- merge( Irsz, Hnk, by = "Helység.megnevezése" )
+IrszHnk <- IrszHnk[!duplicated(IrszHnk), ]
 
-IrszHnk <- IrszHnk[ !duplicated( IrszHnk ), ]
-
-write.csv2( IrszHnk, "IrszHnk.csv", row.names = FALSE )
-saveRDS( IrszHnk, "IrszHnk.rds" )
+write.csv2(IrszHnk, "IrszHnk.csv", row.names = FALSE)
+saveRDS(IrszHnk, "IrszHnk.rds" )
--- README.md
+++ README.md
@@ -2,16 +2,16 @@
 Magyar irányítószámok és települések adatainak összerendelése / Associating Hungarian ZIP codes with data on their settlements.
 
 ## Magyar / Hungarian 
-E szkript célja, hogy - hivatalos adatforrások alapján - hozzárendelje a magyar irányítószámokat a településeikhez, együtt a települések adataival (a Helységnévkönyv alapján). Az adatok forrása az irányítószám esetén a Magyar Posta (https://www.posta.hu/static/internet/download/Iranyitoszam-Internet.XLS), a települési adatok esetén a Központi Statisztikai Hivatal Helységnévkönyve (http://www.ksh.hu/docs/hun/hnk/hnk_2016.xls).
+E szkript célja, hogy - hivatalos adatforrások alapján - hozzárendelje a magyar irányítószámokat a településeikhez, együtt a települések adataival (a Helységnévkönyv alapján). Az adatok forrása az irányítószám esetén a Magyar Posta (https://www.posta.hu/static/internet/download/Iranyitoszam-Internet_uj.xlsx), a települési adatok esetén a Központi Statisztikai Hivatal Helységnévkönyve (https://www.ksh.hu/docs/hun/hnk/hnk_2019.xlsx).
 
-A szkript elvégzi a fenti állományok letöltését, végrehajtja a szükséges adattranszformációkat és a hozzárendelést, majd az eredményt CSV formátumban kimenti.
+A szkript elvégzi a fenti állományok letöltését, végrehajtja a szükséges adattranszformációkat és a hozzárendelést, majd az eredményt CSV és RDS formátumokban kimenti.
 
-A repozitórium a szkripten túl tartalmazza az 2017. 11. 11. dátumra érvényes fenti két bemeneti állományt, és az eredményfájlt is.
+A repozitórium a szkripten túl tartalmazza az 2021. 01. 12. dátumra érvényes fenti két bemeneti állományt, és az eredményfájlt is.
 
 ## Angol / English
 
-The aim of this script is to associate the Hungarian ZIP codes with their respective settlements, along with detailed data on the settlements (using the Detailed Gazetteer), based on official data sources. For the ZIP codes, the data source is the Magyar Posta Ltd., i.e. the Hungarian Post (https://www.posta.hu/static/internet/download/Iranyitoszam-Internet.XLS), for the data on settlements, the data source is the Detailed Gazetteer of the Hungarian Central Statistics Office (http://www.ksh.hu/docs/hun/hnk/hnk_2016.xls).
+The aim of this script is to associate the Hungarian postal codes with their respective settlements, along with detailed data on the settlements (using the Detailed Gazetteer), based on official data sources. For the postal codes, the data source is the Magyar Posta Ltd., i.e. the Hungarian Post (https://www.posta.hu/static/internet/download/Iranyitoszam-Internet_uj.xlsx), for the data on settlements, the data source is the Detailed Gazetteer of the Hungarian Central Statistics Office (https://www.ksh.hu/docs/hun/hnk/hnk_2019.xlsx).
 
-The script downloads the abovementioned data, performs the necessary data transformations and merging, and then saves the result in CSV format.
+The script downloads the abovementioned data, performs the necessary data transformations and merging, and then saves the result in CSV and RDS formats.
 
-In addition to the script itself, the repository contains the abovementioned two input files and the result as of 11-Nov-2017.
+In addition to the script itself, the repository contains the abovementioned two input files and the result as of 12-Jan-2021.
